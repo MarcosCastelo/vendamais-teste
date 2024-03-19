3ae1a1a99b088eb5898f8782d1aa8b94034c3653
MarcosCastelo <marcosvitorcastelo@gmail.com>
2024-03-19 08:03:05 -0300

    feat: create models

--- vendamais/accounts/models.py
+++ vendamais/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from common.exceptions.error_handler import ErrorHandler
 
-# Create your models here.
+class CustomUser(AbstractUser):
+  pass
+
+class Account(models.Model):
+  user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='account')
+  balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+  is_active = models.BooleanField(default=True)
+  
+  def clean(self):
+    if not self.is_active:
+      raise ErrorHandler("The account balance cannot be negative.", code=400)

b984abe3c59e570f393703d165def22e220b2047
MarcosCastelo <marcosvitorcastelo@gmail.com>
2024-03-20 07:31:20 -0300

    feat: create transaction service; add consts for transaction type; validators; add tests

--- vendamais/common/exceptions/error_handler.py
+++
@@ -1,8 +0,0 @@
-class ErrorHandler(Exception):
-  def __init__(self, message, code=None):
-    self.message = message
-    self.code = code
-    super().__init__(message)
-  
-  def to_dict(self):
-    return {'error': self.message, 'code': self.code}
